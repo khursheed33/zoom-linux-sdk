de8b769778df712c2622da558ee27f2b99270bfe
Amol Asodekar <amol.asodekar@e-zest.in>
2025-03-18 15:57:36 +0530

    changes related to multimeeting

--- demo/NetworkConnectionHandler.h
+++ demo/NetworkConnectionHandler.h
@@ -1,8 +1,15 @@
+// NetworkConnectionHandler.h
+#include <network_connection_handler_interface.h> // Zoom SDK header
+#include <functional>
+
+using namespace ZOOMSDK;
+
 class NetworkConnectionHandler : public INetworkConnectionHandler {
     std::function<void()> postToDo_;
 public:
-    NetworkConnectionHandler(std::function<void()> postToDo) : postToDo_(postToDo) {}
-    virtual void onProxyDetectComplete() { if (postToDo_) postToDo_(); }
-    virtual void onProxySettingNotification(IProxySettingHandler* handler) {}
-    virtual void onSSLCertVerifyNotification(ISSLCertVerificationHandler* handler) {}
+    NetworkConnectionHandler(std::function<void()> postToDo);
+
+    virtual void onProxyDetectComplete();
+    virtual void onProxySettingNotification(IProxySettingHandler* handler);
+    virtual void onSSLCertVerifyNotification(ISSLCertVerificationHandler* handler);
 };
