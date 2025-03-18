371ec6863d832e8abcc44c2020ad3b3971f19196
Amol Asodekar <amol.asodekar@e-zest.in>
2025-03-18 18:20:52 +0530

    changes related to multimeeting

--- demo/ZoomSDKAudioRawData.h
+++ demo/ZoomSDKAudioRawData.h
@@ -1,9 +1,9 @@
 // ZoomSDKAudioRawData.h
 #pragma once
 #include "zoom_sdk.h"
+#include "h/zoom_sdk_raw_data_def.h" // Fully defines AudioRawData
 #include "h/rawdata/zoom_rawdata_api.h" // Base raw data API
 #include "h/rawdata/rawdata_audio_helper_interface.h" // Defines IZoomSDKAudioRawDataDelegate
-#include "h/rawdata/rawdata_renderer_interface.h" // Likely defines AudioRawData fully
 #include <functional>
 #include <cstdio> // For FILE
 
