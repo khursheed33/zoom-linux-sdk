fb3546352ab661b7c4ab88e72484e46868523115
Amol Asodekar <amol.asodekar@e-zest.in>
2025-03-19 11:44:11 +0530

    changes related to multimeeting

--- demo/MeetingParticipantsCtrlEventListener.cpp
+++ demo/MeetingParticipantsCtrlEventListener.cpp
@@ -1,3 +1,4 @@
+// MeetingParticipantsCtrlEventListener.cpp
 #include "MeetingParticipantsCtrlEventListener.h"
 #include <iostream>
 
@@ -7,6 +8,14 @@ MeetingParticipantsCtrlEventListener::MeetingParticipantsCtrlEventListener(std::
                                                                            std::function<void()> onIsCoHost)
     : onIsHost_(onIsHost), onIsCoHost_(onIsCoHost) {}
 
+void MeetingParticipantsCtrlEventListener::onUserJoin(ZOOMSDK::IList<unsigned int>* pUserList) {
+    std::cout << "User joined event" << std::endl;
+}
+
+void MeetingParticipantsCtrlEventListener::onUserLeft(ZOOMSDK::IList<unsigned int>* pUserList) {
+    std::cout << "User left event" << std::endl;
+}
+
 void MeetingParticipantsCtrlEventListener::onHostChangeNotification(unsigned int userId) {
     std::cout << "onHostChangeNotification: " << userId << std::endl;
     if (onIsHost_) onIsHost_();
@@ -15,4 +24,12 @@ void MeetingParticipantsCtrlEventListener::onHostChangeNotification(unsigned int
 void MeetingParticipantsCtrlEventListener::onCoHostChangeNotification(unsigned int userId, bool isCoHost) {
     std::cout << "onCoHostChangeNotification: " << userId << ", isCoHost: " << isCoHost << std::endl;
     if (isCoHost && onIsCoHost_) onIsCoHost_();
+}
+
+void MeetingParticipantsCtrlEventListener::onLowOrRaiseHandStatusChanged(bool bLow, unsigned int userid) {
+    std::cout << "Hand status changed for user " << userid << ": " << (bLow ? "low" : "raised") << std::endl;
+}
+
+void MeetingParticipantsCtrlEventListener::onUserNameChanged(ZOOMSDK::IList<unsigned int>* pUserList) {
+    std::cout << "User name changed event" << std::endl;
 }
--- demo/MeetingParticipantsCtrlEventListener.h
+++ demo/MeetingParticipantsCtrlEventListener.h
@@ -1,37 +1,21 @@
 // MeetingParticipantsCtrlEventListener.h
 #pragma once
-#include "zoom_sdk.h" // Foundational SDK header
-#include "h/meeting_service_components/meeting_audio_interface.h" // Define AudioType first
-#include "h/meeting_service_components/meeting_participants_ctrl_interface.h" // Uses AudioType
+#include "meeting_service_components/meeting_participants_ctrl_interface.h"
+#include "meeting_service_components/meeting_audio_interface.h"
 #include <functional>
+#include <iostream>
 
-using namespace ZOOMSDK;
-
-class MeetingParticipantsCtrlEventListener : public IMeetingParticipantsCtrlEvent {
+class MeetingParticipantsCtrlEventListener : public ZOOMSDK::IMeetingParticipantsCtrlEvent {
 private:
     std::function<void()> onIsHost_;
     std::function<void()> onIsCoHost_;
 
 public:
-    MeetingParticipantsCtrlEventListener(std::function<void()> onIsHost, std::function<void()> onIsCoHost);
-
-    virtual void onUserJoin(IList<unsigned int>* lstUserID, const zchar_t* strUserList = NULL) {}
-    virtual void onUserLeft(IList<unsigned int>* lstUserID, const zchar_t* strUserList = NULL) {}
-    virtual void onHostChangeNotification(unsigned int userId); // Declaration only
-    virtual void onLowOrRaiseHandStatusChanged(bool bLow, unsigned int userid) {}
-    virtual void onUserNamesChanged(IList<unsigned int>* lstUserID) {}
-    virtual void onCoHostChangeNotification(unsigned int userId, bool isCoHost); // Declaration only
-    virtual void onInvalidReclaimHostkey() {}
-    virtual void onAllHandsLowered() {}
-    virtual void onLocalRecordingStatusChanged(unsigned int user_id, RecordingStatus status) {}
-    virtual void onAllowParticipantsRenameNotification(bool bAllow) {}
-    virtual void onAllowParticipantsUnmuteSelfNotification(bool bAllow) {}
-    virtual void onAllowParticipantsStartVideoNotification(bool bAllow) {}
-    virtual void onAllowParticipantsShareWhiteBoardNotification(bool bAllow) {}
-    virtual void onRequestLocalRecordingPrivilegeChanged(LocalRecordingRequestPrivilegeStatus status) {}
-    virtual void onAllowParticipantsRequestCloudRecording(bool bAllow) {}
-    virtual void onInMeetingUserAvatarPathUpdated(unsigned int userID) {}
-    virtual void onParticipantProfilePictureStatusChange(bool bHidden) {}
-    virtual void onFocusModeStateChanged(bool bEnabled) {}
-    virtual void onFocusModeShareTypeChanged(FocusModeShareType type) {}
+    MeetingParticipantsCtrlEventListener(std::function<void()> hostCb, std::function<void()> coHostCb);
+    virtual void onUserJoin(ZOOMSDK::IList<unsigned int>* pUserList);
+    virtual void onUserLeft(ZOOMSDK::IList<unsigned int>* pUserList);
+    virtual void onHostChangeNotification(unsigned int userId);
+    virtual void onCoHostChangeNotification(unsigned int userId, bool isCoHost);
+    virtual void onLowOrRaiseHandStatusChanged(bool bLow, unsigned int userid);
+    virtual void onUserNameChanged(ZOOMSDK::IList<unsigned int>* pUserList);
 };
